aeb8194d35d08ee1864a64508a344be9e932981f
tothecorr <thematthewcorr@gmail.com>
2018-11-28 14:04:08 -0500

    added: Driver switch statement

--- Driver.cs
+++ Driver.cs
@@ -1,12 +1,48 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace csharp_file_system
 {
-    class Program
+  class Driver
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-		        // Empty Driver
-        }
+      Console.WriteLine("C# File System");
+      Console.WriteLine("---------------------");
+      string input = Console.ReadLine();
+      switch(input)
+      {
+      case "ls":
+        // Lists Current directory
+      break;
+
+      case "numfile":
+        // Return number of files in the file system
+      break;
+
+      case "addfile":
+        // Add a file
+      break;
+
+      case "addfolder":
+        // Add a file
+      break;
+
+      case "deldir":
+        // Deletes directory
+      break;
+
+      case "delfile":
+        // Deletes file
+      break;
+
+      case "help":
+        // Prints out a list of all possible commands
+      break;
+      }
     }
+  }
 }
